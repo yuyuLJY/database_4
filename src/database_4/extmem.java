c8f34332e7dd432ec8d29241e35d41e387014de6
yu <jiayu1922@163.com>
2019-05-14 20:21:58 +0800

    finish lineFind

--- src/database_4/extmem.java
+++ src/database_4/extmem.java
@@ -1,5 +1,14 @@
 package database_4;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 public class extmem {
 	static int BLOCK_AVAILABLE = 0;
 	static int BLOCK_UNAVAILABLE = 1;
@@ -8,16 +17,22 @@ public class extmem {
 	public int blkSize; /* Block size */
 	public int numAllBlk; /* Number of blocks that can be kept in the buffer */
 	public int numFreeBlk; /* Number of available blocks in the buffer */
-	//public int data; /* Starting address of the buffer */
+	public int data[][];//public int data; /* Starting address of the buffer */
 	
-	public int[][] initBuffer(int bufSize, int blkSize) {
+	public int[][] initBuffer(int bufsize, int blksize) {
 		/**No.1
 		 * 初始化缓冲区，其输入参数
 		 * @param bufSize为缓冲区大小（单位：字节）
 		 * @param blkSize为块的大小（单位：字节）
 		 * @return  若缓冲区初始化成功，则该函数返回指向该缓冲区的地址；否则，返回NULL。
 		 */
-		int data[][] = new int[bufSize][bufSize/bufSize];
+		numIO = 0;
+		bufSize = bufsize;//512+8
+		blkSize = blksize+1;//65
+		numAllBlk = bufSize / blkSize ;
+		numFreeBlk = numAllBlk;
+		System.out.printf("缓冲区行和列%d %d\n",bufSize/blkSize,blkSize);
+		data = new int[bufSize/blkSize][bufSize];
 		return data;
 	}
 	
@@ -25,15 +40,24 @@ public class extmem {
 		/**No.2
 		 * 释放缓冲区buf占用的内存空间。
 		 */
+		data = null;
 	}
 	
-	public int getNewBlockInBuffer(){
+	public Object getNewBlockInBuffer(){
 		/**No.3
 		 * 在缓冲区buf中申请一个新的块。
 		 * @return 若申请成功，则返回该块的地址的块数号；否则，返回NULL
 		 */
 		int blk=0;
-		return blk;
+		for(int i = 0;i<numAllBlk;i++) {//遍历所有的缓冲块
+			//System.out.printf("%d 块的标记 %d\n",i,data[i][blkSize-1]);
+			if(data[i][blkSize-1]==BLOCK_AVAILABLE) {//可以用
+				data[i][blkSize-1] = BLOCK_UNAVAILABLE;//标记这个块不可以用了
+				numFreeBlk--;
+				return i;
+			}
+		}
+		return null;
 	}
 	
 	public void freeBlockInBuffer(int blk) {
@@ -41,7 +65,13 @@ public class extmem {
 		 * @param blk缓冲区中块的地址的块数号
 		 * 解除块blk对缓冲区内存的占用，即将blk占据的内存区域标记为可用
 		 */
-	
+		//Step1:将该条缓冲里边的东西删掉
+		for(int i=0;i<bufSize;i++) {
+			data[blk][i]=0;
+		}
+		//Step2:解除占用标志位
+		data[blk][blkSize] = BLOCK_AVAILABLE;//重新标记这个块可以用了
+		numFreeBlk++;
 	}
 	
 	public int dropBlockOnDisk(int addr) {
@@ -49,25 +79,86 @@ public class extmem {
 		 * 从磁盘上删除地址为addr的磁盘块内的数据。
 		 * @return 若删除成功，则返回0；否则，返回-1。
 		 */
+		//TODO 
 		return 0;
 	}
 	
-	public void readBlockFromDisk(int addr){
+	public Object readBlockFromDisk(int blk,int addr){
 		/**No.6
 		 * 将磁盘上地址为addr的磁盘块读入缓冲区buf。同时，缓冲区buf的I/O次数加1。
 		 *@param addr磁盘上地址
-		 *@return 若读取成功，则返回缓冲区内该块的地址(->改成块里边的内容)；否则，返回NULL。
+		 *@return 若读取成功，则返回缓冲区内该块的地址blk；否则，返回NULL。
 		 */
-		
+		//如果缓冲区没有位置了，从磁盘读取不成功
+	    if (numFreeBlk == 0)
+	    {
+	        System.out.println("Buffer Overflows!\n");
+	        return null;
+	    }
+	
+	    //找到一个空的缓冲区
+	    //int blk = (int) getNewBlockInBuffer();
+	    data[blk][blkSize] = BLOCK_UNAVAILABLE;//标记这个块不可以用了
+	    //把disk的东西读进缓存区	
+		String fileName = "src/disk/lineFind/"+String.valueOf(addr)+".txt";
+	    try {
+			FileReader fr = new FileReader(fileName);
+			BufferedReader bf = new BufferedReader(fr);
+			String str;
+			// 按行读取字符串
+			int i= 0;
+			while ((str = bf.readLine()) != null) {
+				data[blk][i] = Integer.valueOf(str);//把内容写进缓冲区
+				i++;
+			}
+			bf.close();
+			fr.close();
+		} catch (IOException e) {
+			System.out.println("Reading Block Failed!\n");
+			e.printStackTrace();
+			return null;
+		}
+	    //更改一些信息
+	    numFreeBlk--;
+	    numIO++;
+		return blk;
 	}
 	
-	public int writeBlockToDisk(int blkPtr, int addr) {
+	public int writeBlockToDisk(int blkPtr, int addr) throws IOException {
 		/**No.7 
 		 * 将缓冲区buf内的块blk写入磁盘上地址为addr的磁盘块。同时，缓冲区buf的I/O次数加1。
 		 * @param blkPtr为buf块的位置
 		 * @param addr为磁盘上地址
 		 * @return 若写入成功，则返回0；否则，返回-1。
 		 */
-		return 1;
+		//创建文件夹，写文件
+		String address = String.valueOf(addr);
+		File fout = new File("src/disk/lineFind/"+address+".txt");
+		FileOutputStream fos;
+		try {
+			fos = new FileOutputStream(fout);
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+			 
+			//0 表示的是元组 1 表示的是后继的地址
+			//0 12 34
+			for (int i = 0; i <65; i++) {
+				String s = String.valueOf(data[blkPtr][i]);
+				bw.write(s);
+				bw.newLine();
+			}
+			bw.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Writing Block Failed!\n");
+			e.printStackTrace();
+	        return -1;
+		}
+		//Step1:将该条缓冲里边的东西删掉
+		for(int i=0;i<bufSize;i++) {
+			data[blkPtr][i]=0;
+		}
+		data[blkPtr][blkSize]= BLOCK_AVAILABLE;//64是是否可用的标志位
+	    numFreeBlk++;
+	    numIO++;
+		return 0;
 	}
 }
--- src/database_4/mainP.java
+++ src/database_4/mainP.java
@@ -1,16 +1,40 @@
 package database_4;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Random;
 
 public class mainP {
-	public static void main(String[] args) {
-		extmem t = new extmem();
-		int bufSize = 520;//buffer缓冲区的大小
-		int blkSize = 64;//块的大小
-		t.initBuffer(bufSize, blkSize);//初始化缓冲区
-		//TODO 生成R和S的数据
+	static int sTupleNumber = 224;
+	static int rTupleNumber = 112;
+	static int sTable[][] = new int[sTupleNumber][2];
+	static int rTable[][] = new int[rTupleNumber][2];
+	static extmem ex = new extmem();
+	
+	public static void main(String[] args) throws IOException {
+		int bufsize = 520;//buffer缓冲区的大小
+		int blksize = 64;//块的大小
+		ex.initBuffer(bufsize, blksize);//初始化缓冲区
+		//生成R和S的数据
 		createTableData();
-		//TODO
+		//TODO 应用搜索算法：线性搜索  R.A=40
+		LineSearch();
+		
+		//TODO 应用搜索算法：二分搜索
+		//TODO 应用搜索算法：B+树
+		//TODO 投影 
+		//TODO 
+		/*
+		System.out.println("-------------函数Main-----------------");
+		sortData();
+		for(int i=0;i<rTupleNumber;i++) {
+			for(int j = 0;j<2;j++) {
+				System.out.print(rTable[i][j]+" ");
+			}
+			System.out.print("\n");
+		}*/
 	}
 	
 	static void createTableData() {
@@ -61,8 +85,150 @@ public class mainP {
 			System.out.print("\n");
 		}
 		*/
-		
-		//TODO 直接从文件里边读进去
-		
+
+		String sfileName = "src/table/Stable.txt";
+		String rfileName = "src/table/Rtable.txt";
+		rTable = readData(rfileName,rTupleNumber);
+		sTable = readData(sfileName,sTupleNumber);
+		//验证读取的效果
+		/*
+		System.out.println("打印S表的值");
+		for(int i=0;i<sTupleNumber;i++) {
+			for(int j = 0;j<2;j++) {
+				System.out.print(sTable[i][j]+" ");
+			}
+			System.out.print("\n");
+		}
+		System.out.println("----------------------------------");
+		System.out.println("打印R表的值");
+		for(int i=0;i<rTupleNumber;i++) {
+			for(int j = 0;j<2;j++) {
+				System.out.print(rTable[i][j]+" ");
+			}
+			System.out.print("\n");
+		}*/
 	}
+	
+	static int[][] readData(String fileName,int TupleNumber) {
+		//TODO 读取文件里边的table信息
+		int table[][] = new int[TupleNumber][2];
+		try {
+			FileReader fr = new FileReader(fileName);
+			BufferedReader bf = new BufferedReader(fr);
+			String str;
+			// 按行读取字符串
+			int i = 0;
+			while ((str = bf.readLine()) != null) {
+				String s[] = str.split(" ");
+				table[i][0] = Integer.valueOf(s[0]);
+				table[i][1] = Integer.valueOf(s[1]);
+				i++;
+			}
+			bf.close();
+			fr.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return table;
+	}
+
+	static void LineSearch() throws IOException {
+		/**
+		 * 线性搜索，R.A=40。先排序，然后按照大小顺序放进磁盘，装满
+		 */
+		System.out.println("-------------函数LineSearch-----------------");
+		//Step1:先进行排序
+		sortData();
+		/*
+		for(int i=0;i<rTupleNumber;i++) {
+			for(int j = 0;j<2;j++) {
+				System.out.print(rTable[i][j]+" ");
+			}
+			System.out.print("\n");
+		}*/
+		//Step2：先写进缓冲，再写进磁盘
+		int diskOrder=-1;
+		for(int i=0;i<rTupleNumber;i=i+7) {//行
+			//System.out.printf("遍历第%d次\n",i);
+			Object lineNumber = ex.getNewBlockInBuffer();
+			if(lineNumber!=null) {//缓冲区没有满
+				int target = 0;//标记，赋值缓冲区到什么位置了
+				int line = (int) lineNumber;
+				//System.out.printf("申请到的列标：%d\n",line);
+				for(int j = i;j<i+7;j++){
+					ex.data[line][target]=ex.data[line][target+1]=
+					ex.data[line][target+2]=ex.data[line][target+3]=rTable[j][0];
+					ex.data[line][target+4]=ex.data[line][target+5]=
+					ex.data[line][target+6]=ex.data[line][target+7]=rTable[j][1];
+					target+=8;
+				}
+			}else {
+				//System.out.printf("缓冲空间用完了\n");
+				//把缓冲区的内容写进磁盘
+				for(int k=0;k<8;k++) {
+					diskOrder++;
+					ex.writeBlockToDisk(k,diskOrder);
+				}
+				i = i-7;
+			}
+		}
+		//Step3：开始查找R.A=40
+		//遍历缓冲区有记录的地方，如果找不到，就清空缓冲区，再从缓冲区读进来
+		int flag = 0;
+		while(flag!=1) {//还没找到
+			//先去缓冲区找
+			for(int i = 0;i<8;i++) {
+				if(ex.data[i][64]==1) {//有数据
+					for(int j = 0;j<56;j=j+8) {
+						int oneA = ex.data[i][j];
+						if(oneA==40) {//找到了
+							System.out.printf("找到数据!!!在缓冲区的%d块 index[%d,%d]\n",i,j,j+3);
+							flag = 1;
+						}
+					}
+				}
+			}
+			//缓冲区没有,先释放缓冲区，从磁盘写进缓冲区
+			if(flag==0) {
+				for(int i = 0;i<8;i++) {
+					ex.freeBlockInBuffer(i); 
+				}
+				//理想是读进8块，但是不一定有8块
+				for(int i = 0;i<8 && diskOrder>=0;i++) {
+					ex.readBlockFromDisk(i,diskOrder);
+					diskOrder--;
+				}
+			}
+		}
+		System.out.printf("线性查找 IO次数：%d\n",ex.numIO);
+		
+		//打印查看效果
+		System.out.printf("-------------查看缓冲区------------\n");
+		int dataCopy[][]=ex.data;
+		for(int i = 0;i<8;i++) {
+			for(int j = 0;j<65;j++) {
+				System.out.printf(dataCopy[i][j]+" ");
+			}
+			System.out.printf("\n");
+		}
+	}
+	
+	static void sortData(){
+		/**
+		 *对二维数组的第一列，进行简单的选择排序 
+		 */
+		for(int i=0;i<rTupleNumber;i++) {
+			for(int j = 0;j<rTupleNumber;j++) {
+				if(rTable[i][0]>rTable[j][0]) {//第一个数字>第二个数字
+					int temp0=rTable[i][0];
+					rTable[i][0] = rTable[j][0];
+					rTable[j][0] = temp0;
+					int temp1=rTable[i][1];
+					rTable[i][1] = rTable[j][1];
+					rTable[j][1] = temp1;
+				}
+			}
+		}
+	}
+	
 }
